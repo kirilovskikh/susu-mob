32576fd2c3ff7b65628f330ff6a43b39316864eb
Kirilovskikh Maksim <kirilovskikh@gmail.com>
2017-05-13 02:31:21 +0500

    update button

--- app/src/main/java/com/kirilovskikh/labs/MainActivity.kt
+++ app/src/main/java/com/kirilovskikh/labs/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.kirilovskikh.labs
 
-import android.widget.Button
-import butterknife.BindView
 import butterknife.OnClick
 
 class MainActivity : BaseActivity() {
-
-    @BindView(R.id.lab1Button) lateinit var but: Button
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
