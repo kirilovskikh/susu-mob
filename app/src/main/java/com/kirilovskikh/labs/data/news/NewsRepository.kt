6bd1a20de585835e7ca4c1a00460727ace73cfed
Kirilovskikh Maksim <kirilovskikh@gmail.com>
2017-05-13 03:02:14 +0500

    add fetch observable

--- app/src/main/java/com/kirilovskikh/labs/data/news/NewsRepository.kt
+++ app/src/main/java/com/kirilovskikh/labs/data/news/NewsRepository.kt
@@ -1,27 +1,32 @@
 package com.kirilovskikh.labs.data.news
 
 import com.kirilovskikh.labs.data.NewsModel
+import rx.Observable
 
 /**
  * Created by kirilovskikh on 13.05.17.
  */
 class NewsRepository {
 
-    fun fetch(lastModel: NewsModel?) {
-        val items = mutableListOf<NewsModel>()
+    fun fetch(lastModel: NewsModel?): Observable<List<NewsModel>> {
+        return Observable.create<List<NewsModel>> { subscriber ->
+            val items = mutableListOf<NewsModel>()
 
-        val startId = lastModel?.id ?: 1
-        val endId = startId + 20
+            val startId = lastModel?.id ?: 1
+            val endId = startId + 20
 
-        for (i in startId..endId) {
+            for (i in startId..endId) {
 
-            val model = if (i % 4 == 0L) {
-                NewsModel(i, image = "https://hi-news.ru/wp-content/uploads/2017/01/asteroid-650x387.jpg")
-            } else {
-                NewsModel(i, text = "$i $i $i $i")
+                val model = if (i % 4 == 0L) {
+                    NewsModel(i, image = "https://hi-news.ru/wp-content/uploads/2017/01/asteroid-650x387.jpg")
+                } else {
+                    NewsModel(i, text = "$i $i $i $i")
+                }
+
+                items.add(model)
             }
 
-            items.add(model)
+            subscriber.onNext(items)
         }
 
     }
