c341eeebfe28f9b8b7e57832fb1eeb6ab6b24ad4
Kirilovskikh Maksim <kirilovskikh@gmail.com>
2017-05-13 02:40:50 +0500

    add lab4 activity

---
+++ app/src/main/java/com/kirilovskikh/labs/activities/Lab4Activity.kt
@@ -0,0 +1,16 @@
+package com.kirilovskikh.labs.activities
+
+import com.kirilovskikh.labs.R
+
+/**
+ * Created by kirilovskikh on 13.05.17.
+ */
+class Lab4Activity : BaseActivity() {
+
+    override fun getLayoutId(): Int = R.layout.lab_4_activity
+
+    override fun onCreate() {
+
+    }
+
+}
