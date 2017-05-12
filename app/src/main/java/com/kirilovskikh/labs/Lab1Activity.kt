dbc1e16fe63e70d26c5d59863d65a2b27b42f9ab
Kirilovskikh Maksim <kirilovskikh@gmail.com>
2017-05-12 21:11:23 +0500

    add base variant lab 1

--- app/src/main/java/com/kirilovskikh/labs/Lab1Activity.kt
+++ app/src/main/java/com/kirilovskikh/labs/Lab1Activity.kt
@@ -1,12 +1,19 @@
 package com.kirilovskikh.labs
 
 import android.content.Context
+import android.widget.EditText
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.OnClick
 import org.jetbrains.anko.intentFor
 
 /**
  * Created by kirilovskikh on 12.05.17.
  */
 class Lab1Activity : BaseActivity() {
+
+    @BindView(R.id.textView) lateinit var textView: TextView
+    @BindView(R.id.editText) lateinit var editText: EditText
 
     companion object {
         fun newIntent(context: Context) = context.intentFor<Lab1Activity>()
@@ -15,6 +22,11 @@ class Lab1Activity : BaseActivity() {
     override fun getLayoutId(): Int = R.layout.lab_1_activity
 
     override fun onCreate() {
+    }
+
+    @OnClick(R.id.applyButton)
+    fun onApplyButtonClick() {
+        textView.text = editText.text
     }
 
 }
